72672341be3bab77ac68c140ee19027e274e529e
igorxciv <icheliadinski@gmail.com>
2022-06-26 11:46:06 -0400

    feat: add styles for checkbox states

--- libs/eva/src/lib/atoms/checkbox/Checkbox.tsx
+++ libs/eva/src/lib/atoms/checkbox/Checkbox.tsx
@@ -12,17 +12,17 @@ export const Checkbox: React.FC<Props> = (props) => {
 	const classNames = cn(
 		'border w-5 h-5 rounded-[3px] cursor-pointer transition flex justify-center items-center peer-checked:bg-check bg-center bg-no-repeat peer-focus:shadow-ctrl-both',
 		{
-			'border-basic-500 bg-basic-600/[.08] peer-focus:border-basic-600 hover:border-primary hover:bg-primary/[.16] peer-checked:bg-primary peer-checked:border-primary peer-checked:hover:bg-primary-400 peer-checked:hover:border-primary-400':
+			'border-basic-500 bg-basic-600/[.08] peer-focus:border-basic-600 peer-focus:bg-basic-600/[.24] peer-focus:hover:bg-basic-600/[.08] hover:border-primary hover:bg-primary/[.16] peer-checked:bg-primary peer-checked:border-primary peer-checked:hover:bg-primary-400 peer-checked:hover:border-primary-400 peer-checked:peer-active:bg-primary-600 peer-checked:peer-active:border-primary-600 peer-checked:peer-focus:bg-primary-600 peer-checked:peer-focus:border-primary-700':
 				color === 'basic',
-			'border-primary-500 bg-primary-600/[.08] hover:bg-primary/[.16] peer-checked:bg-primary peer-checked:border-primary peer-checked:hover:bg-primary-400 peer-checked:hover:border-primary-400':
+			'border-primary-500 bg-primary-600/[.08] peer-focus:bg-primary-500/[.24] peer-focus:hover:bg-primary-500/[.08] hover:bg-primary/[.16] peer-checked:bg-primary peer-checked:border-primary peer-checked:hover:bg-primary-400 peer-checked:hover:border-primary-400 peer-checked:peer-active:bg-primary-600 peer-checked:peer-active:border-primary-600 peer-checked:peer-focus:bg-primary-600 peer-checked:peer-focus:border-primary-700':
 				color === 'primary',
-			'border-success-500 bg-success-600/[.08] hover:bg-success/[.16] peer-checked:bg-success peer-checked:border-success peer-checked:hover:bg-success-400 peer-checked:hover:border-success-400':
+			'border-success-500 bg-success-600/[.08] peer-focus:bg-success-500/[.24] peer-focus:hover:bg-success-500/[.08] hover:bg-success/[.16] peer-checked:bg-success peer-checked:border-success peer-checked:hover:bg-success-400 peer-checked:hover:border-success-400 peer-checked:peer-active:bg-success-600 peer-checked:peer-active:border-success-600 peer-checked:peer-focus:bg-success-600 peer-checked:peer-focus:border-success-700':
 				color === 'success',
-			'border-info-500 bg-info-600/[.08] hover:bg-info/[.16] peer-checked:bg-info peer-checked:border-info peer-checked:hover:bg-info-400 peer-checked:hover:border-info-400':
+			'border-info-500 bg-info-600/[.08] peer-focus:bg-info-500/[.24] peer-focus:hover:bg-info-500/[.08] hover:bg-info/[.16] peer-checked:bg-info peer-checked:border-info peer-checked:hover:bg-info-400 peer-checked:hover:border-info-400 peer-checked:peer-active:bg-info-600 peer-checked:peer-active:border-info-600 peer-checked:peer-focus:bg-info-600 peer-checked:peer-focus:border-info-700':
 				color === 'info',
-			'border-warning-500 bg-warning-600/[.08] hover:bg-warning/[.16] peer-checked:bg-warning peer-checked:border-warning peer-checked:hover:bg-warning-400 peer-checked:hover:border-warning-400':
+			'border-warning-500 bg-warning-600/[.08] peer-focus:bg-warning-500/[.24] peer-focus:hover:bg-warning-500/[.08] hover:bg-warning/[.16] peer-checked:bg-warning peer-checked:border-warning peer-checked:hover:bg-warning-400 peer-checked:hover:border-warning-400 peer-checked:peer-active:bg-warning-600 peer-checked:peer-active:border-warning-600 peer-checked:peer-focus:bg-warning-600 peer-checked:peer-focus:border-warning-700':
 				color === 'warning',
-			'border-danger-500 bg-danger-600/[.08] hover:bg-danger/[.16] peer-checked:bg-danger peer-checked:border-danger peer-checked:hover:bg-danger-400 peer-checked:hover:border-danger-400':
+			'border-danger-500 bg-danger-600/[.08] peer-focus:bg-danger-500/[.24] peer-focus:hover:bg-danger-500/[.08] hover:bg-danger/[.16] peer-checked:bg-danger peer-checked:border-danger peer-checked:hover:bg-danger-400 peer-checked:hover:border-danger-400 peer-checked:peer-active:bg-danger-600 peer-checked:peer-active:border-danger-600 peer-checked:peer-focus:bg-danger-600 peer-checked:peer-focus:border-danger-700':
 				color === 'danger',
 		},
 	)
